99ce6324afd48bc2e966eaafc4e4730c0ec4b05a
kanhubikash <kanhubikash@gmail.com>
2019-03-09 01:23:25 +0530

    first commit

---
+++ FriendsGroup/src/main/java/com/friendsgroup/friends/FriendsGroupApplication.java
@@ -0,0 +1,13 @@
+package com.friendsgroup.friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsGroupApplication.class, args);
+	}
+
+}
